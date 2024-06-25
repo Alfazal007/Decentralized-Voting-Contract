3714ac51032fd9ddfdee5baa64800237d68a98a4
Alfazal007 <alfazal7777@gmail.com>
2024-06-25 15:07:06 +0530

    updated contracts

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -35,6 +35,7 @@ contract Voting is Ownable {
     mapping(address => bool) voterMapping;
 
     mapping(address => uint256) candidatesToVoters;
+    address[] public candidatesArray;
 
     function addAdmin(address newAdmin) public onlyOwner {
         if (hasAdmin(newAdmin) == true || newAdmin == ownerAddress) {
@@ -60,6 +61,12 @@ contract Voting is Ownable {
         return voterMapping[voterToBeFound] == true;
     }
 
+    function hasCandidates(
+        address candidateToBeFound
+    ) public view returns (bool) {
+        return candidatesToVoters[candidateToBeFound] == 1;
+    }
+
     // start adding candidates -- admins
     function startAddingCandidates() public {
         if (hasAdmin(msg.sender) == true) {
@@ -69,9 +76,17 @@ contract Voting is Ownable {
 
     // add candidates -- admins
     function addCandidates(address newCandidate) public {
+        if (candidatesAddingStarted == false) {
+            return;
+        }
         if (hasAdmin(msg.sender) != true) {
             return;
         }
+        if (hasCandidates(newCandidate) == true) {
+            return;
+        }
+        candidatesToVoters[newCandidate] = 1;
+        candidatesArray.push(newCandidate);
     }
 
     // end adding candidates -- admins
@@ -80,8 +95,62 @@ contract Voting is Ownable {
             candidatesAddingStarted = false;
         }
     }
+
     // start voting -- admins
+    function startVoting() public {
+        if (hasAdmin(msg.sender) != true) {
+            return;
+        }
+        if (candidatesAddingStarted == true) {
+            return;
+        }
+        voingStarted = true;
+    }
+
     // cast a vote -- voters
+    function castVote(address candidate) public {
+        if (voingStarted == false) {
+            return;
+        }
+        if (hasCandidates(candidate) == false) {
+            return;
+        }
+        // has already voted
+        if (voterMapping[msg.sender] == false) {
+            return;
+        }
+        voterMapping[msg.sender] = false;
+        candidatesToVoters[candidate]++;
+    }
+
     // end voting -- admins
+    function endVoting() public {
+        if (hasAdmin(msg.sender) != true) {
+            return;
+        }
+        voingStarted = false;
+    }
+
     // winner declaration
+
+    // need to do many things here
+    function declareWinner() public returns (address, uint256) {
+        address winner = address(0);
+        uint256 votes = 0;
+        if (voingStarted == true) {
+            return (winner, votes);
+        }
+
+        for (uint256 i = 0; i < candidatesArray.length; i++) {
+            address curCandidate = candidatesArray[i];
+            if (candidatesToVoters[curCandidate] > votes) {
+                votes = candidatesToVoters[curCandidate];
+                winner = curCandidate;
+            }
+            delete candidatesToVoters[curCandidate];
+        }
+        voters = new Person[](0);
+        candidatesArray = new address[](0);
+        return (winner, votes);
+    }
 }
