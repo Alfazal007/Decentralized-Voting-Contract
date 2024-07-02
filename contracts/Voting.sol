c71e6adf8976a521baa9c25f5298bc8bac15e75a
Alfazal007 <alfazal7777@gmail.com>
2024-07-02 16:24:01 +0530

    tested on remix

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -5,16 +5,23 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 error IsOwner_Or_AlreadyAnAdmin();
-error AlreadyVoter_Or_IsAdmin_Or_IsOwner();
+error AlreadyVoter_Or_IsAdmin_Or_IsOwner_Or_IsCandidate();
 error Candidates_Cannot_Be_Added_Yet();
 error Not_Admin();
 error Already_A_Candidate();
 error No_Candidate();
 error Voting_Not_Started();
 error Not_A_Candidate();
-error Already_Voted();
+error Already_Voted_Or_Not_A_Voter();
 error Not_Admin_Or_Candidates_Still_Adding();
 error StillVoting_Or_Candidates_Still_Adding();
+error Voting_Already_Started();
+error Admin_Cannot_Be_Candidate();
+error Candidates_Still_Adding();
+error There_Are_Already_Some_Candidates();
+error No_Voters();
+error Only_Admin_Can_Add_Voters();
+error No_Candidates_Or_No_Voters();
 
 contract Voting is Ownable {
     // this is shared resource
@@ -40,15 +47,16 @@ contract Voting is Ownable {
     }
 
     // varibles
-    Person[] admins;
-    mapping(address => bool) adminMapping;
+    Person[] public admins;
+    mapping(address => bool) public adminMapping;
 
-    Person[] voters;
-    mapping(address => bool) voterMapping;
+    Person[] public voters;
+    mapping(address => bool) public voterMapping;
 
-    mapping(address => uint256) candidatesToVoters;
+    mapping(address => uint256) public candidatesToVoters;
     address[] public candidatesArray;
 
+    // test
     function addAdmin(address newAdmin) public onlyOwner {
         if (hasAdmin(newAdmin) == true || newAdmin == ownerAddress) {
             revert IsOwner_Or_AlreadyAnAdmin();
@@ -57,15 +65,28 @@ contract Voting is Ownable {
         admins.push(Person(PersonType.ADMIN, newAdmin));
     }
 
+    // test
     function hasAdmin(address adminToBeFound) public view returns (bool) {
         return adminMapping[adminToBeFound] == true;
     }
 
-    function addVoter(address newVoter) public onlyOwner {
+    function addVoter(address newVoter) public {
         if (
-            hasVoter(newVoter) || hasAdmin(newVoter) || newVoter == ownerAddress
+            hasVoter(newVoter) ||
+            hasAdmin(newVoter) ||
+            newVoter == ownerAddress ||
+            hasCandidates(newVoter)
         ) {
-            revert AlreadyVoter_Or_IsAdmin_Or_IsOwner();
+            revert AlreadyVoter_Or_IsAdmin_Or_IsOwner_Or_IsCandidate();
+        }
+        if (votingStarted == true) {
+            revert Voting_Already_Started();
+        }
+        if (hasAdmin(msg.sender) == false) {
+            revert Only_Admin_Can_Add_Voters();
+        }
+        if (candidatesAddingStarted == true) {
+            revert Candidates_Still_Adding();
         }
         voterMapping[newVoter] = true;
         voters.push(Person(PersonType.VOTERS, newVoter));
@@ -78,14 +99,18 @@ contract Voting is Ownable {
     function hasCandidates(
         address candidateToBeFound
     ) public view returns (bool) {
-        return candidatesToVoters[candidateToBeFound] == 1;
+        return candidatesToVoters[candidateToBeFound] >= 1;
     }
 
     // start adding candidates -- admins
     function startAddingCandidates() public {
-        if (hasAdmin(msg.sender) == true) {
-            candidatesAddingStarted = true;
+        if (candidatesArray.length > 0) {
+            revert There_Are_Already_Some_Candidates();
         }
+        if (hasAdmin(msg.sender) == false) {
+            revert Not_Admin();
+        }
+        candidatesAddingStarted = true;
     }
 
     // add candidates -- admins
@@ -98,6 +123,9 @@ contract Voting is Ownable {
         }
         if (hasCandidates(newCandidate) == true) {
             revert Already_A_Candidate();
+        }
+        if (hasAdmin(newCandidate) == true) {
+            revert Admin_Cannot_Be_Candidate();
         }
         candidatesToVoters[newCandidate] = 1;
         candidatesArray.push(newCandidate);
@@ -116,10 +144,13 @@ contract Voting is Ownable {
             revert Not_Admin();
         }
         if (candidatesAddingStarted == true) {
-            revert Candidates_Cannot_Be_Added_Yet();
+            revert Candidates_Still_Adding();
         }
         if (candidatesArray.length == 0) {
             revert No_Candidate();
+        }
+        if (voters.length <= 0) {
+            revert No_Voters();
         }
         votingStarted = true;
     }
@@ -134,9 +165,9 @@ contract Voting is Ownable {
         }
         // has already voted
         if (voterMapping[msg.sender] == false) {
-            revert Already_Voted();
+            revert Already_Voted_Or_Not_A_Voter();
         }
-        voterMapping[msg.sender] = true;
+        voterMapping[msg.sender] = false;
         candidatesToVoters[candidate]++;
     }
 
@@ -151,12 +182,15 @@ contract Voting is Ownable {
     // winner declaration
 
     // need to do many things here
-    function declareWinner() public returns (address, uint256) {
-        address winner = address(0);
-        uint256 votes = 0;
+    function declareWinner() public onlyOwner returns (address, uint256) {
         if (votingStarted == true || candidatesAddingStarted == true) {
             revert StillVoting_Or_Candidates_Still_Adding();
         }
+        if (candidatesArray.length <= 0 || voters.length <= 0) {
+            revert No_Candidates_Or_No_Voters();
+        }
+        address winner = address(0);
+        uint256 votes = 0;
 
         for (uint256 i = 0; i < candidatesArray.length; i++) {
             address curCandidate = candidatesArray[i];
@@ -176,6 +210,6 @@ contract Voting is Ownable {
             delete adminMapping[admins[i].person];
         }
         delete admins;
-        return (winner, votes);
+        return (winner, votes - 1);
     }
 }
